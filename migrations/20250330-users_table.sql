ae713f6cfdcf9bb9084b34db4b0dc2c5f6467d19
Josh Reyes <josh@joshreyes.com>
2025-03-30 21:48:19 -0400

    Make a database, migrations, and users table

---
+++ migrations/20250330-users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT NOT NULL UNIQUE,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+) STRICT;
