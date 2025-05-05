ba1e0fc363ab374cc41ac244f9c113a7b28fd9f6
Josh Reyes <josh@joshreyes.com>
2025-05-05 09:49:57 -0400

    Getting a donation flow going

--- bin/app.js
+++ bin/app.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 const config = require("config");
+const donation_routes = require("../lib/donation_routes.js");
 const express = require("express");
 const hbs = require("express-handlebars");
 const path = require("node:path");
+
 
 const HBS = hbs.create({
     extname: "hbs",
     defaultLayout: "base",
 });
 const PORT = config.get("app.port");
+const SESSION_SECRET = config.get("session.secret");
 const VIEWS_DIRPATH = path.join(__dirname, "..", "views");
 
 const app = express();
@@ -18,8 +21,22 @@ app.engine("hbs", HBS.engine);
 app.set("view engine", "hbs");
 app.set("views", VIEWS_DIRPATH);
 
+// Middleware
 app.use(express.static("static"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+    },
+});
+
+// Routes
 app.get("/", (req, res, next) => res.render("home"));
+app.use("/donate", donation_routes);
 
 if(require.main === module) {
     app.listen(
