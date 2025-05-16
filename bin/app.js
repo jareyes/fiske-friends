cef18fb9776e07ab5430792e84049ffa08d01b84
Joshua Reyes <josh@joshreyes.com>
2025-05-16 17:03:26 +0000

    Fix things to make cookies work in production

--- bin/app.js
+++ bin/app.js
@@ -27,13 +27,15 @@ app.use(express.static("static"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.set("trust proxy", 1)
+app.set("trust proxy", 1);
 app.use(session({
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
         secure: SESSION_SECURE,
+	sameSite: "strict",
+	domain: "fiskefriends.org",
     },
 }));
 
